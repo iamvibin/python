d597775dabf9d90658fb4ec65a2715bd8981eaa3
Vibin <availableforvibin43@gmail.com>
2018-04-09 08:33:00 +0530

    commiting changes

--- urllib1.py
+++ urllib1.py
@@ -4,6 +4,7 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
 for line in fhand:
     print(line.decode().strip()) #decode is used to convert the byte code into unicode in which python stores its string
+
 #print the count of different words in the file
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 counts = dict()
